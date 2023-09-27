337887b8f6a69d7e9ae0920d5399c7b4d4456b9a
ewudes <ewudes@gmail.com>
2023-09-27 16:16:31 +0300

    fix: app js

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import "./jquery.fullPage.js";
+import "./contacts.js";
+import "./form.js";
+import "./greeting.js";
+import "./menu.js";
+import "./reviews.js";
+import "./slider.js";
+import "./team.js";
