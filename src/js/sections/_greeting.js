9dd3ca5eeb7a7fe3fecf5e9307983cda760d11b4
ewudes <ewudes@gmail.com>
2024-04-17 18:49:33 +0300

    add: open hamburger menu

--- src/js/sections/_greeting.js
+++ src/js/sections/_greeting.js
@@ -1,50 +1,20 @@
 (function() {
 	"use strict";
-  const openButton = document.querySelector(".hamburger-menu-link");
 
-  openButton.addEventListener("click", function (event) {
-    event.preventDefault(); //nav--large-width
-    var mainMenu = document.getElementsByClassName("nav")[0].cloneNode(true);
-    mainMenu.classList.remove("nav--large-width");
-    mainMenu.classList.add("nav--small-width");
-    const overlay = openOverlay(mainMenu.outerHTML);
-    document.body.appendChild(overlay);
+  const navList = document.querySelector(".header__nav-list");
+  const navButton = document.querySelector(".hamburger");
+
+  function handleNoneScroll() {
+    return window.scrollTo({top: 0});
+  }
+
+  navButton.addEventListener("click", function() {
+    navList.classList.toggle("header__nav-list-active-menu");
+
+    if (navList.classList.contains("header__nav-list-active-menu")) {
+      window.addEventListener("scroll", handleNoneScroll);
+    } else {
+      window.removeEventListener("scroll", handleNoneScroll);
+    }
   });
 })();
-
-
-
-// function openOverlay(content) {
-//   const overlayElement = document.createElement("div");
-//   overlayElement.classList.add("overlay");
-
-//   const containerElement = document.createElement("div");
-//   containerElement.classList.add("container");
-
-//   const contentElement = document.createElement("div");
-//   contentElement.classList.add("content");
-//   contentElement.innerHTML = content;
-
-//   const closeMenu = function () {
-//     document.body.removeChild(overlayElement);
-//   };
-
-//   const closeElement = document.createElement("a");
-//   closeElement.classList.add("close");
-//   closeElement.textContent = "x";
-//   closeElement.href = "#";
-//   closeElement.addEventListener("click", closeMenu);
-
-//   const menuItems = contentElement.querySelectorAll("li");
-
-//   for (let item of menuItems) {
-//     item.addEventListener("click", closeMenu);
-//   }
-
-//   overlayElement.appendChild(containerElement);
-//   containerElement.appendChild(closeElement);
-//   containerElement.appendChild(contentElement);
-
-//   return overlayElement;
-// }
-
